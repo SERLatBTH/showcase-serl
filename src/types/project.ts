69d4305f706934bd2f8a130f88e68f8ef5e37e41
Andreas Bauer <dev@andreasbauer.org>
2025-01-13 22:58:45 +0100

    Change Project interface to Zod type

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,10 +1,13 @@
-export interface Project {
-  id: string
-  title: string
-  description: string
-  type: string
-  tags: string[]
-  screenshots: string[]
-  urlGitHub: string
-  urlHomepage: string
-}
+import { z } from 'zod'
+
+const project = z.object({
+  title: z.string().nonempty(),
+  description: z.string().nonempty(),
+  type: z.string(),
+  tags: z.string().array(),
+  urlGitHub: z.string().url(),
+  urlHomepage: z.string().url(),
+  urlScreenshots: z.string().array(),
+})
+
+export type Project = z.infer<typeof project>
