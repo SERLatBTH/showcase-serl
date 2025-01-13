df1fc999a1a09c92b465f66e589005ca31d7eba6
Andreas Bauer <dev@andreasbauer.org>
2025-01-13 22:32:57 +0100

    Add basic structure

---
+++ src/types/project.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  id: string
+  title: string
+  description: string
+  type: string
+  tags: string[]
+  screenshots: string[]
+  urlGitHub: string
+  urlHomepage: string
+}
