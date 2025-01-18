ba1493fb2f3e76168822e9b20c29cda78414d767
Andreas Bauer <dev@andreasbauer.org>
2025-01-18 15:34:21 +0100

    Adjust overview page to read from available proj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { availableProjects } from '@/data/available-projects'
 import {
   Card,
   CardContent,
@@ -8,30 +9,33 @@ import {
 } from '@/components/ui/card'
 
 export default function Home() {
+  availableProjects
   return (
     <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
       <h1 className='text-xl font-bold'>Welcome to SERL Showcase.</h1>
       <div className='grid'>
-        <Card>
-          <CardHeader>
-            <CardTitle>Project Title</CardTitle>
-            <CardDescription>Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </CardContent>
-          <CardFooter>
-            <p>Tags</p>
-          </CardFooter>
-        </Card>
+        {availableProjects.map((p) => (
+          <Card key={p.title}>
+            <CardHeader>
+              <CardTitle>{p.title}</CardTitle>
+              <CardDescription>{p.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </CardContent>
+            <CardFooter>
+              <p>Tags</p>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
     </div>
   )
