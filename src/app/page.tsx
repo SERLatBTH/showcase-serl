231f35a26bc7cde65524b43f84ba713fd5205483
Andreas Bauer <dev@andreasbauer.org>
2025-01-20 23:49:41 +0100

    Add placeholder image to project card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import Image from 'next/image'
 
 export default function Home() {
-  availableProjects
   return (
     <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
       <h1 className='text-xl font-bold'>Welcome to SERL Showcase.</h1>
-      <div className='grid'>
+      <div className='grid gap-4'>
         {availableProjects.map((p) => (
           <Card key={p.title}>
             <CardHeader>
@@ -21,18 +21,28 @@ export default function Home() {
               <CardDescription>{p.description}</CardDescription>
             </CardHeader>
             <CardContent>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+              <div className='flex flex-col items-center gap-4 sm:flex-row'>
+                <Image
+                  src='/fallback_image/No_image_available.svg'
+                  alt='Project image'
+                  width={300}
+                  height={300}
+                />
+
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div>
             </CardContent>
             <CardFooter>
-              <p>Tags</p>
+              <p>Tags: {p.tags.join(', ')}</p>
             </CardFooter>
           </Card>
         ))}
